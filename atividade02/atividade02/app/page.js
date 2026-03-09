3d2409ffa123c112dd4ccd8b6206743d3acb8d7a
sofia-viana <sofiavianax9@gmail.com>
2026-03-09 18:16:50 +0000

    atividade 02

---
+++ atividade02/atividade02/app/page.js
@@ -0,0 +1,11 @@
+import Profile from "../components/Profile";
+
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", padding: "40px" }}>
+      <h1>Site criado para a disciplina Programação Front-End</h1>
+
+      <Profile />
+    </main>
+  );
+}
---
+++ atividade02/atividade02/components/Profile.js
@@ -0,0 +1,20 @@
+export default function Profile() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "20px" }}>
+      <img
+        src="/foto.jpg"
+        alt="Foto de Sofia"
+        width="200"
+        style={{ borderRadius: "50%" }}
+      />
+
+      <h2>Sofia Viana</h2>
+
+      <p>
+        Estudante de Sistemas para Internet com interesse em desenvolvimento
+        web e qualidade de software (QA). Gosto de aprender novas tecnologias
+        e criar soluções simples e eficientes para problemas do dia a dia.
+      </p>
+    </div>
+  );
+}
